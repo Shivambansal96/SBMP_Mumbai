b626a493b27c51f80a65491ef811d9486a2013d4
Shivam Bansal <bansal.shivam1216@gmail.com>
2026-08-17 14:34:55 +0530

    first commit

---
+++ day1.py
@@ -0,0 +1,363 @@
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # var1  = 5
+
+# # # # # # # # n = None
+
+# # # # # # # # arr = [32, 322]
+
+# # # # # # # # print(2 ** 4)
+
+
+# # # # # # # # print(2 == 2 and 4 == 2)  # False
+# # # # # # # # print(2 == 2 or 4 == 2) # True
+
+
+# # # # # # # # print(2 == 2 and True)  # False
+
+# # # # # # # # print(2 + 4)
+
+
+
+# # # # # # # # # ---------------------------------------------- #
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+# # # # # # # # print("My name is", name, sep=" = ")
+# # # # # # # # print("My name is \nShivam" , sep=" = ")
+
+
+
+# # # # # # # # a = 5
+# # # # # # # # b = 10
+# # # # # # # # sum = a + int(b)
+# # # # # # # # print(sum)
+
+# # # # # # # side = int(input())
+# # # # # # # print("Area =", side ** 2)
+
+# # # # # # # a = float(input("Enter a num: "))
+# # # # # # # b = float(input("Enter another num: "))
+# # # # # # # avg = (a + b)/ 2
+# # # # # # # print("Average =", avg)
+
+
+# # # # # # # a = float(input("Enter a num: "))
+# # # # # # # b = float(input("Enter another num: "))
+# # # # # # # print(a>=b)
+
+
+
+# # # # # # name = "Shivam"
+# # # # # # age = 27
+# # # # # # city = "Hyderabad"
+
+# # # # # # print("My name is", name, ", I am", age, "years old and live in", city)
+
+
+
+
+# # # # # # print(f"My name is {name}, I am {age} years old and I live in {city}.")
+
+# # # # # c = int(input("Enter degree in Celsius: "))
+
+# # # # # f = ( c * 9/5) + 32
+
+# # # # # print(f"Celsius: {c}")
+# # # # # print("After Conversion")
+# # # # # print(f"Fahrenheit: °{f}")­
+
+
+# # # # # s = "hello"
+# # # # # a = "world"
+
+# # # # # print(s + " " + a)
+
+# # # #   #  012345
+# # # # s = "Shivam"
+
+# # # # a = s[1:len(s)-1]
+# # # # b = s[len(s)-2:0:-1]
+
+# # # # print(b)
+
+
+# # # # originalStr = "racecar"
+# # # # reversedStr = originalStr[::-1]
+
+# # # # print(originalStr == reversedStr)
+
+# # # # s = "I am a Coder."
+
+# # # # print(s.swapcase())
+# # # # print(s.lower())
+# # # # print(s.upper())
+# # # # print(s.capitalize())
+# # # # print(s.count("Code"))
+# # # # print(s.endswith("Cer."))
+# # # # print(s.find("z"))
+# # # # print(s.index("z"))
+# # # # print(s.replace("a", "i", 1))
+
+# # # age = 70
+
+# # # if(age >= 18):
+# # #     print("ADULT")
+# # # else:
+# # #     if(age > 60):
+# # #         print("Senior Citizen")
+# # #     else:
+# # #         print("Minor")
+
+# # # color = input("Enter the color = ").capitalize()
+
+# # # if(color == "Red"):
+# # #     print("STOP")
+# # # elif(color == "Green"):
+# # #     print("GO")
+# # # elif(color == "Yellow"):
+# # #     print("Ready To Go!!")
+# # # else:
+# # #     print("Invalid Input")
+
+
+# # marks = 7
+
+# # if(marks >= 90 and marks <= 100):
+# #     grade = "A"
+# # elif(marks >= 80):
+# #     grade = "B"
+# # elif(marks >= 70):
+# #     grade = "C"
+# # elif(marks >= 60):
+# #     grade = "D"
+# # else:
+# #     grade = "F"
+
+# # print(grade)
+
+# num = int(input("Enter a num: "))
+# ans = ""
+
+# if(num > 0):
+#     ans = "Positive"
+# elif(num < 0):
+#     ans = "Negative"
+# else:
+#     ans = "Zero"
+
+# print(f"{num} is {ans}")
+
+m1 = 13
+m2 = 53
+m3 = 5
+
+if(m1 > m2 and m1 > m3):
+    print(f"{m1} is the greatest")
+elif(m2 > m3):
+    print(f"{m2} is the greatest")
+else:
+    print(f"{m3} is the greatest")
